77e2679cb64aa8d2326681eab3c0464c81c02180
Whitespace404 <rush2asharahul@gmail.com>
2022-01-19 20:36:46 +0530

    Added average time to stats commands

--- stats.py
+++ stats.py
@@ -16,3 +16,22 @@ def correct_vs_attempts():
     conn.commit()
     conn.close()    
     return str(correct_answers), str(attempts)
+
+def average_time():
+    conn = sqlite3.connect("stats.db")
+    cursor = conn.cursor()
+    stats = cursor.execute("""SELECT * FROM stats """).fetchall()
+
+    times = []
+    count = 0
+
+    for stat in stats:
+        times.append(stat[4])
+        count += 1
+    
+    summation = 0
+    for time in times:
+        summation += float(time)
+
+    average = summation / count
+    return str(round(average, 2)) + " seconds."
